a0d2f77742f75450110ca1efe2e58db1e4aade1f
fazimfm <fazimwrkid@gmail.com>
2025-12-24 11:03:50 +0530

    Add push script and ensure all portfolio files are included

---
+++ push.bat
@@ -0,0 +1,1 @@
+echo "# updated-portfolio" >> README.md & git init & git add README.md & git commit -m "first commit" & git branch -M main & git remote add origin https://github.com/fazimfm/updated-portfolio.git & git push -u origin main
